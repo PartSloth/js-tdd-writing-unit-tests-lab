ba71ad5aa8a6686e2eda0e4622dacb64d8361ca7
Louiza Mak <louiza.mak@gmail.com>
2024-03-05 21:47:04 -0600

    complete lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,47 @@
-// Your tests here
+import {isPalindrome} from "../utils";
+
+describe("isPalindrome", () => {
+    it("returns true if string is a palindrome, false if it isn't", () => {
+        const word = "racecar";
+        const check = isPalindrome(word);
+        expect(check).toBe(true);
+    });
+
+    it("returns true if string is a palindrome, false if it isn't", () => {
+        const word = "car";
+        const check = isPalindrome(word);
+        expect(check).toBe(false);
+    });
+
+    it("returns true if string is a palindrome, false if it isn't", () => {
+        const word = "cAr";
+        const check = isPalindrome(word);
+        expect(check).toBe(false);
+    });
+
+    it("returns true if string is a palindrome, false if it isn't", () => {
+        const word = "raCEcar";
+        const check = isPalindrome(word);
+        expect(check).toBe(true);
+    });
+
+    it("returns false for an empty string", () => {
+        const word = "";
+        const check = isPalindrome(word);
+        expect(check).toBe(false);
+    });
+
+    it("throw an error if input has non-alphabetic characters", ()=> {
+        const word = "rac3car"
+        expect(() => {
+            isPalindrome(word);
+        }).toThrow("String cannot contain non-alphabetic characters!")
+    });
+
+    it("throw an error if input is not a string", () => {
+        const word = 10;
+        expect(() => {
+            isPalindrome(word)
+        }).toThrow("This is not a string!")
+    });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,14 @@
-// Your code here
+export function isPalindrome(word) {
+    if (typeof word !== "string") {
+        throw ("This is not a string!")
+    }
+    const reverse = word.split("").reverse().join("");
+    if (word === "") {
+        return false;
+    } 
+    if (!/^[A-Za-z]+$/.test(word)) {
+        throw "String cannot contain non-alphabetic characters!";
+    }
+
+    return word.toLowerCase() === reverse.toLowerCase();
+};
